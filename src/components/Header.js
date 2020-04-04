f26f31f3321b7113c03948b4909038521270ed67
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-04-04 11:24:49 +0200

    Setup project with Gatsby & Leaflet maps

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import Container from 'components/Container';
+import Container from "components/Container";
 
 const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
+        <p>COVID-19 MAP</p>
         <ul>
           <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2/">Page 2</Link>
+            <Link to="/">Map</Link>
           </li>
         </ul>
       </Container>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
-import 'assets/stylesheets/application.scss';
+import "assets/stylesheets/application.scss";
 
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import Header from "components/Header";
+
+import Footer from "components/Footer";
 
 const Layout = ({ children, pageName }) => {
-  let className = '';
+  let className = "";
 
-  if ( pageName ) {
+  if (pageName) {
     className = `${className} page-${pageName}`;
   }
 
@@ -21,7 +22,8 @@ const Layout = ({ children, pageName }) => {
       </Helmet>
       <div className="wrapper">
         <Header />
-        <main>{ children }</main>
+
+        <main>{children}</main>
         <Footer />
       </div>
     </>
@@ -30,7 +32,7 @@ const Layout = ({ children, pageName }) => {
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-  pageName: PropTypes.string
+  pageName: PropTypes.string,
 };
 
 export default Layout;

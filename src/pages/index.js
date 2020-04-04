fb6e88f4be10a7f0438a4f5f9ad4ce5f90000ece
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-04-04 12:47:31 +0200

    add layers with pointers

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,12 @@ import L from "leaflet";
 import axios from "axios";
 
 import Layout from "components/Layout";
-import Container from "components/Container";
 import Map from "components/Map";
-
 import { CORONA_PATH } from "../data/paths";
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -29,6 +27,7 @@ const IndexPage = () => {
 
     const { data = [] } = response;
     const hasData = Array.isArray(data) && data.length > 0;
+
     if (!hasData) return;
 
     const geoJson = {
@@ -38,17 +37,63 @@ const IndexPage = () => {
         const { lat, long: lng } = countryInfo;
         return {
           type: "Feature",
+          properties: {
+            ...country,
+          },
           geometry: {
             type: "Point",
             coordinates: [lng, lat],
           },
-          properties: {
-            ...country,
-          },
         };
       }),
     };
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`;
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: "icon",
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo(map);
   }
+
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: "OpenStreetMap",
@@ -59,12 +104,10 @@ const IndexPage = () => {
   return (
     <Layout pageName="home">
       <Helmet>
-        <title>COVID-19 Map</title>
+        <title>Home Page</title>
       </Helmet>
 
-      <Map {...mapSettings}></Map>
-
-      <Container type="content" className="text-center home-start"></Container>
+      <Map {...mapSettings} />
     </Layout>
   );
 };
